09b6d6fd3f2376b18c1372c8f3db766f602d5531
ThiagoProvi <thiago@provi.com.br>
2020-10-11 10:33:08 -0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,97 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  if(!title || !url || (techs && !techs.length)) {
+    return response.status(400).json({error: 'Missing information'})
+  }
+
+  if(!url.includes("github.com")){
+    return response.status(400).json({error: 'invalid URL'})
+  }
+
+  if(!techs.length){
+    return response.status(400).json({error: 'You must have to pass at least one language'})
+  }
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const { title, url, techs } = request.body
+
+  if(url && !url.includes("github.com")){
+    return response.status(400).json({error: 'invalid URL'})
+  }
+
+  if(!repositories.length) {
+    return response.status(404).json({error: 'No repository found'})
+  }
+  
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if(repoIndex < 0) {
+    return response.status(400).json({error: 'Repository not found'})
+  }
+
+  const oldRepo = repositories[repoIndex]
+  
+  const newRepo = {
+    id,
+    title: title || oldRepo.title,
+    url: url || oldRepo.url,
+    techs: techs || oldRepo.techs,
+    likes: repositories[repoIndex].likes
+  }
+
+  repositories[repoIndex] = newRepo
+
+  return response.json(newRepo)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if(repoIndex < 0) {
+    return response.status(400).json({error: 'Repository not found'})
+  }
+
+  repositories.splice(repoIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+  
+  if(repoIndex < 0) {
+    return response.status(400).json({error: 'Repository not found'})
+  }
+
+  repositories[repoIndex].likes++
+
+  return response.json(repositories[repoIndex])
 });
 
 module.exports = app;
